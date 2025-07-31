2c69ddfe145c9761113259a91ad3e1c6a4e883ef
ADAning <695439722@qq.com>
2025-07-31 08:31:02 +0800

    tui support (WIP)

--- src/tui/widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""TUI Widgets for Animal Well Flute"""
-
-from .song_display import SongDisplayWidget
-from .flute_fingering import FluteFingeringWidget
-from .song_table import SongTable
-
-__all__ = ["SongDisplayWidget", "FluteFingeringWidget", "SongTable"]
